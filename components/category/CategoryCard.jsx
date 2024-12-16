37c2a177355374a98d32871d690a7112e533148b
dipto <dipto.bd.r@gmail.com>
2024-12-16 16:14:26 +0600

    images added to the product catergory cards

--- components/category/CategoryCard.jsx
+++ components/category/CategoryCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function CategoryCard( {image, title, price} ) {
   return (
-    <div className="bg-gray-200 p-5 w-full flex flex-col items-center">
+    <div className="p-5 w-full flex flex-col items-center custom-shadow">
         <Image src={image} alt={title} width={350} height={400}/>
         <p>{title}</p>
         <p>{price}</p>
