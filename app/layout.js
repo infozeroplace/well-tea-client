4c380b070696b12f90be07bee97d59351d2af510
dipto <dipto.bd.r@gmail.com>
2024-12-16 22:21:19 +0600

    Redux setup done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono, Prompt } from "next/font/google";
 import "./globals.css";
+import { ReduxProvider } from "@/store/Provider";
 import { Header, Footer, Navbar } from "@/components";
+
+const fonts = `${geistSans.variable} ${geistMono.variable}`;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,19 +23,22 @@ const prompt = Prompt({
 
 export const metadata = {
   title: "Well Tea",
-  description: "Well Tea",
+  description: "Well Tea - Fresh and Organic Tea Products",
+  keywords: "tea, organic tea, well tea, premium tea",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fonts} antialiased`}
       >
-        <Header />
-        <Navbar />
-        {children}
-        <Footer />
+        <ReduxProvider>
+          <Header />
+          <Navbar />
+          {children}
+          <Footer />
+        </ReduxProvider>
       </body>
     </html>
   );
