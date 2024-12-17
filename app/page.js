fda4c00c4ffab4e5ee554f56ed7f33955b55a6a9
Ruman-Islam <rumanislam48@gmail.com>
2024-12-18 00:36:36 +0600

    test

--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
-"use client"
-
 import { Banner, Category, Hero } from "@/components";
-import { useGetTestQuery } from "@/services/features/auth/authApi";
 
 export default function Home() {
-  const { data, isLoading } = useGetTestQuery({});
-
   return (
     <div className="">
       <Hero />
