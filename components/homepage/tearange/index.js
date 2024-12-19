36d94fd4a4cfbdc4e28d73ac56ca140830b4504d
Hridoy <hridoy007.lu@gmail.com>
2024-12-20 01:46:53 +0600

    Discover tearange component modified

--- components/homepage/tearange/index.js
+++ components/homepage/tearange/index.js
@@ -1,20 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const discoverItems = [
   { title: "Harbal", image: "/images/welltea_hero.png" },
-  { title: "Blac Tea", image: "/images/welltea_hero.png" },
+  { title: "Blac Tea", image: "/images/discover_image.jpeg" },
   { title: "Green Tea", image: "/images/welltea_hero.png" },
-  { title: "White Tea", image: "/images/welltea_hero.png" },
+  { title: "White Tea", image: "/images/discover_image.jpeg" },
   { title: "Oolong Tea", image: "/images/welltea_hero.png" },
-  { title: "Matcha", image: "/images/welltea_hero.png" },
+  { title: "Matcha", image: "/images/discover_image.jpeg" },
 ];
 const TeaRange = () => {
+  const [bgimage, setBgimage] = useState("/images/welltea_hero.png");
+
+  const handleImage = (image) => {
+    setBgimage(image);
+  };
   return (
     <div className="relative my-14">
       <div className="w-full h-[500px]">
-        <Image layout="fill" src="/images/welltea_hero.png" alt="Discover" />
+        <Image layout="fill" src={bgimage} alt="Discover" />
       </div>
       <h2 className="absolute top-[15%] left-[6%] text-white text-4xl">
         Discover Our Tea Range
@@ -24,6 +31,9 @@ const TeaRange = () => {
           {discoverItems.map((item) => (
             <div key={item?.title} className="w-full">
               <div
+                onMouseEnter={() => {
+                  handleImage(item?.image);
+                }}
                 className="flex gap-3 items-end group text-white cursor-pointer w-full"
               >
                 <div className="w-[2px] h-16 group-hover:h-24 duration-400 bg-white"></div>
