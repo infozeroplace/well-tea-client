7fbcbcafb4b19e0cf863689d12fa92503b132895
Hridoy <hridoy007.lu@gmail.com>
2024-12-28 02:29:52 +0600

    Common link button has added

--- components/homepage/giftitems/index.jsx
+++ components/homepage/giftitems/index.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import GiftItemCard from "./GiftItemCard";
 import { productList } from "@/data/products";
-import { SectionButton } from "@/components/shared";
+import { SectionButton, SectionLinkButton } from "@/components/shared";
 
 const newitems = [
   {
@@ -54,90 +54,8 @@ const newitems = [
     price: "50",
     discountPrice: "20",
   },
-  {
-    id: "5",
-    designation: "best sellar",
-    discount: "40",
-    image: "/products/product_05_square.jpg",
-    hoverImage: "/products/product_06.jpg",
-    type: "Organic Teas",
-    title: "White Tera Rose Melange",
-    rating: "4",
-    price: "50",
-    discountPrice: "20",
-  },
-  {
-    id: "6",
-    designation: "best sellar",
-    discount: "40",
-    image: "/products/product_07_square.jpg",
-    hoverImage: "/products/product_08.jpg",
-    type: "Organic Teas",
-    title: "White Tera Rose Melange",
-    rating: "4",
-    price: "50",
-    discountPrice: "20",
-  },
-  {
-    id: "7",
-    designation: "best sellar",
-    discount: "40",
-    image: "/products/product_05_square.jpg",
-    hoverImage: "/products/product_06.jpg",
-    type: "Organic Teas",
-    title: "White Tera Rose Melange",
-    rating: "4",
-    price: "50",
-    discountPrice: "20",
-  },
-  {
-    id: "8",
-    designation: "best sellar",
-    discount: "40",
-    image: "/products/product_07_square.jpg",
-    hoverImage: "/products/product_08.jpg",
-    type: "Organic Teas",
-    title: "White Tera Rose Melange",
-    rating: "4",
-    price: "50",
-    discountPrice: "20",
-  },
-  {
-    id: "9",
-    designation: "best sellar",
-    discount: "40",
-    image: "/products/product_05_square.jpg",
-    hoverImage: "/products/product_06.jpg",
-    type: "Organic Teas",
-    title: "White Tera Rose Melange",
-    rating: "4",
-    price: "50",
-    discountPrice: "20",
-  },
-  {
-    id: "10",
-    designation: "best sellar",
-    discount: "40",
-    image: "/products/product_05_square.jpg",
-    hoverImage: "/products/product_06.jpg",
-    type: "Organic Teas",
-    title: "White Tera Rose Melange",
-    rating: "4",
-    price: "50",
-    discountPrice: "20",
-  },
 ];
 const GiftItems = () => {
-  const size = newitems.length;
-  const [itemLimit, setItemLimit] = useState(4);
-
-  const handleLimit = () => {
-    if (itemLimit < size) setItemLimit(itemLimit + 4);
-    else {
-      console.log("No More Items");
-      alert("No More Items");
-    }
-  };
   return (
     <div className="section-gap bg-teagreen-100 py-10">
       <div className="container">
@@ -149,13 +67,12 @@ const GiftItems = () => {
             <h4 className="text-center md:text-left text-4xl">Gift Products</h4>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-2 lg:gap-4">
-            {newitems.map(
-              (item, idx) =>
-                idx < itemLimit && <GiftItemCard key={item?.id} item={item} />
-            )}
+            {newitems.map((item, idx) => (
+              <GiftItemCard key={item?.id} item={item} />
+            ))}
           </div>
           <div className="flex justify-center md:justify-center text-lg">
-            <SectionButton title="See More" onClick={handleLimit} />
+            <SectionLinkButton title="See More" url="/tea" />
           </div>
         </div>
       </div>
