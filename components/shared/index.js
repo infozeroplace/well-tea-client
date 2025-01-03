a4fcd9341c7b67aff2591c52dd05f9faefb18d9c
Ruman-Islam <rumanislam48@gmail.com>
2025-01-03 16:59:27 +0600

    done

--- components/shared/index.js
+++ components/shared/index.js
@@ -1,3 +1,4 @@
 export {default as SectionButton} from './SectionButton';
 export {default as SectionLinkButton} from './SectionLinkButton';
 export {default as SectionTitle} from './SectionTitle';
+export {default as StarRating} from './StarRating';
