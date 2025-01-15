a377f0ed4dce0ac30da8e706719ca20aaa8e0804
dipto <dipto.bd.r@gmail.com>
2025-01-15 18:17:15 +0600

    Product card updated

--- components/shared/ProductCard.jsx
+++ components/shared/ProductCard.jsx
@@ -52,8 +52,6 @@ const ProductCard = ({ product }) => {
     };
   },[])
 
-  console.log(cardRef);
-
   return (
     <div
       // onMouseLeave={() => {
@@ -106,17 +104,19 @@ const ProductCard = ({ product }) => {
 
           {/* Product Content */}
           <div
-            style={{ display: addButtonClicked ? "none" : "block" }}
-            className="text-center w-full duration-400 mx-auto px-3"
+            // style={{ display: addButtonClicked ? "none" : "block" }}
+            className={`w-full transition-all duration-300 mx-auto px-3 ${
+              addButtonClicked ? "opacity-0" : "opacity-1"
+            }`}
           >
             <div className="">
               <h4 className="md:text-sm lg:text-base text-start font-normal text-teagreen-800 h-10 overflow-hidden text-ellipsis whitespace-nowrap">
                 {product?.title}
               </h4>
-              <div className="flex justify-between items-center py-1">
-                <p className="capitalize text-sm">{product?.format}</p>
+              {/* <div className="flex justify-between items-center py-1"> */}
+              <p className="capitalize text-sm">{product?.format}</p>
 
-                {product?.isSale ? (
+              {/* {product?.isSale ? (
                   <div className="flex justify-center gap-2 font-semibold text-teagreen-800">
                     <div>£{product?.unitPrices[0]?.salePrice}</div>
                     <del>£{product?.unitPrices[0]?.price}</del>
@@ -125,30 +125,43 @@ const ProductCard = ({ product }) => {
                   <div className="font-semibold text-teagreen-800">
                     £{product?.unitPrices[0]?.price}
                   </div>
-                )}
-              </div>
+                )} */}
+              {/* </div> */}
             </div>
           </div>
         </Link>
         {/* Add to card */}
         <div
-          className={`border-t transform w-full mx-auto transition-all duration-400 ${
-            addButtonClicked ? "hidden" : "block"
+          className={`border-t transform w-full mx-auto transition-all duration-300 ${
+            addButtonClicked ? "opacity-0" : "opacity-1"
           }`}
         >
           <button
             onClick={handleWeight}
-            className="uppercase text-xs py-3 text-center w-full flex items-center justify-center text-teagreen-800 hover:bg-teagreen-400 transition-all duration-400 gap-2"
+            className="uppercase text-xs py-3 px-5 w-full flex items-center justify-center text-teagreen-800 hover:bg-teagreen-400 transition-all duration-400 gap-2"
           >
-            <CiShoppingCart size={20} />
-            <span>add to cart</span>
+            {/* <CiShoppingCart size={20} className="" /> */}
+            <CiShoppingCart size={20} className="absolute top-1/2 left-[22px] -translate-y-1/2" />
+            {/* <span>add to cart</span> */}
+            <div className="">
+              {product?.isSale ? (
+                <div className="flex justify-center text-center gap-2 font-semibold text-teagreen-800">
+                  <div>£{product?.unitPrices[0]?.salePrice}</div>
+                  <del>£{product?.unitPrices[0]?.price}</del>
+                </div>
+              ) : (
+                <div className="font-semibold text-teagreen-800">
+                  £{product?.unitPrices[0]?.price}
+                </div>
+              )}
+            </div>
           </button>
         </div>
 
         {/* Weight Selection*/}
         <div
-          className={`absolute botton-0 left-0 w-full bg-inherit overflow-hidden transition-height duration-500 ${
-            addButtonClicked ? "h-fit" : "h-0"
+          className={`absolute botton-0 h-fit left-0 z-10 w-full transform transition-all duration-300 ${
+            addButtonClicked ? "-translate-y-full opacity-1" : "translate-y-0 opacity-0"
           }`}
         >
           <p className="uppercase text-center py-1 text-sm border-b">
