90e878820a4d5ae247859bc50e4c5f0580e5f903
Hridoy <hridoy007.lu@gmail.com>
2025-02-09 17:48:52 +0600

    Adderss section has updated

--- app/(pages)/profile/components/EditAddress.jsx
+++ app/(pages)/profile/components/EditAddress.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {
   Button,
+  Checkbox,
+  Form,
   Input,
   Modal,
   ModalBody,
@@ -14,13 +16,27 @@ import {
 import { countries } from "./countriesData";
 
 const EditAddress = ({ user, isOpen, onOpenChange }) => {
+  const [action, setAction] = useState();
   const [addressData, setAddressData] = useState({
     address1: "",
     address2: "",
+    isSelected: false,
   });
 
+
+   // Checkbox toggle
+   const handleChecked = (e) => {
+    setAddressData({ ...addressData, isSelected: e.target.checked });
+  };
+
+   // Updating form data state
   const handleInput = (field, value) =>
     setAddressData((prev) => ({ ...prev, [field]: value }));
+ 
+  const handleSubmit =  (e) => {
+    
+  };
+ 
   return (
     <Modal
       isOpen={isOpen}
@@ -34,55 +50,122 @@ const EditAddress = ({ user, isOpen, onOpenChange }) => {
               Edit Address
             </ModalHeader>
             <ModalBody className="gap-6">
-              <Input
-                className=""
-                variant="bordered"
-                label="Company"
-                type="text"
-              />
-              <Input
-                className=""
-                variant="bordered"
-                label="Address 1"
-                type="text"
-                isRequired
-                onChange={(e) => {
-                  handleInput("address1", e.target.value);
-                }}
-              />
-              <Input
-                className=""
-                variant="bordered"
-                label="Address 2"
-                type="text"
-              />
-              <Select
-                isRequired
-                className=""
-                variant="bordered"
-                defaultSelectedKeys={["gb"]}
-                label="Choose Country"
-                placeholder="Select an country"
+              <Form
+                className="w-full flex gap-6"
+                validationBehavior="native"
+                onReset={() => setAction("reset")}
+                onSubmit={handleSubmit}
               >
-                {countries.map((country) => (
-                  <SelectItem key={country.key}>{country.label}</SelectItem>
-                ))}
-              </Select>
+                <div className="flex flex-col md:flex-row gap-6 justify-between w-full">
+                  <Input
+                    variant="bordered"
+                    isRequired
+                    errorMessage="Please enter your first name"
+                    label="First Name"
+                    labelPlacement="inside"
+                    name="firstName"
+                    type="text"
+                  />
+                  <Input
+                    variant="bordered"
+                    errorMessage="Please enter a your last name"
+                    label="Last Name"
+                    labelPlacement="inside"
+                    name="lastName"
+                    type="text"
+                  />
+                </div>
 
-              <Input
-                className=""
-                variant="bordered"
-                label="City"
-                type="text"
-                isRequired
-              />
-
-              <Input
-                className=""
-                variant="bordered"
-                label="Postal Code"
-                type="text"
-              />
+                <Input
+                  variant="bordered"
+                  // isRequired
+                  // errorMessage="Please enter your company"
+                  label="Company"
+                  labelPlacement="inside"
+                  name="company"
+                  type="text"
+                />
+                <Input
+                  variant="bordered"
+                  isRequired
+                  errorMessage="Please enter your address"
+                  label="Address 1"
+                  labelPlacement="inside"
+                  name="address1"
+                  type="text"
+                />
+                <Input
+                  variant="bordered"
+                  label="Address 2"
+                  labelPlacement="inside"
+                  name="address2"
+                  type="text"
+                />
+                <Input
+                  variant="bordered"
+                  isRequired
+                  errorMessage="Please enter your city"
+                  label="City"
+                  labelPlacement="inside"
+                  name="city"
+                  type="text"
+                />
+               
+                <Select
+                  variant="bordered"
+                  isRequired
+                  errorMessage="Please select your country"
+                  label="Country"
+                  name="country"
+                  // selectionMode="multiple"
+                >
+                  {countries.map((country) => (
+                    <SelectItem key={country} name="country">
+                      {country}
+                    </SelectItem>
+                  ))}
+                </Select>
+                <Input
+                  variant="bordered"
+                  isRequired
+                  errorMessage="Please enter your postal code"
+                  label="Postal Code"
+                  labelPlacement="inside"
+                  name="postalCode"
+                  type="text"
+                />
+                <Input
+                  variant="bordered"
+                  isRequired
+                  errorMessage="Please enter your phone number"
+                  label="Phone"
+                  labelPlacement="inside"
+                  name="phone"
+                  type="text"
+                />
+                <Checkbox
+                  isSelected={addressData?.isSelected}
+                  onChange={handleChecked}
+                  className="custom-checkbox"
+                >
+                  <span className="text-sm font-extralight">
+                    Set as default address
+                  </span>
+                </Checkbox>
+                <div className="flex gap-2">
+                  <Button type="submit" className="bg-teagreen-600 text-white">
+                    Submit
+                  </Button>
+                  <Button type="reset" variant="flat">
+                    Reset
+                  </Button>
+                </div>
+                {action && (
+                  <div className="text-small text-default-500">
+                    Action: <code>{action}</code>
+                  </div>
+                )}
+              </Form>
             </ModalBody>
             <ModalFooter>
               <Button color="danger" variant="light" onPress={onClose}>
