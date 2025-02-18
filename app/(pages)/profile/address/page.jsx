7f2c5f2ac2826f2d68011c58c58c780fb98b9682
Ruman-Islam <rumanislam48@gmail.com>
2025-02-18 06:51:32 +0600

    profile modified

--- app/(pages)/profile/address/page.jsx
+++ app/(pages)/profile/address/page.jsx
@@ -1,23 +1,24 @@
 "use client";
 
+import LoadingOverlay from "@/components/shared/LoadingOverlay";
 import {
   useDeleteAddressMutation,
   useGetAddressQuery,
 } from "@/services/features/address/addressApi";
+import { useEditProfileMutation } from "@/services/features/profile/profileApi";
 import { useAppSelector } from "@/services/hook";
 import { useDisclosure } from "@heroui/react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { FiPlusCircle } from "react-icons/fi";
 import { GoKey } from "react-icons/go";
-import { useEditProfileMutation } from "@/services/features/profile/profileApi";
 import {
   AddNewAddress,
+  ConfirmDelete,
   EditAddress,
   EditPassword,
   EditProfile,
-  ConfirmDelete
 } from "../components";
-import { toast } from "react-hot-toast";
-import LoadingOverlay from "@/components/shared/LoadingOverlay";
 
 function AddressScreen() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -25,7 +26,10 @@ function AddressScreen() {
   const [selectedAddress, setSelectedAddress] = useState({});
   const [addressId, setAddressId] = useState("");
 
-  const [editProfile, { data: editProfileData, isLoading: editProfileLoading }] = useEditProfileMutation();
+  const [
+    editProfile,
+    { data: editProfileData, isLoading: editProfileLoading },
+  ] = useEditProfileMutation();
 
   const {
     auth: { user, token },
@@ -64,28 +68,38 @@ function AddressScreen() {
   }, [editProfileData]);
 
   return (
-    <div className="w-full p-4">
-      <div className="p-6 bg-teagreen-100 section-gap rounded-lg">
-        <p className="text-2xl content-gap">Personal Information</p>
-        <div className="content-gap">
-          <p className="mb-3">
-            {user?.firstName} {user?.lastName}
-          </p>
-          <p className="mb-3">
-            <span className="font-semibold">Email:</span> {user?.email}
-          </p>
-          <p className="content-gap">
-            <span className="font-semibold">Phone:</span> {user?.phone}
-          </p>
+    <div className="w-full p-4 flex flex-col gap-5">
+      <div className="p-6 bg-teagreen-100 rounded">
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-1">
+            <p className="text-brand__font__size__lg mb-1">
+              Personal Information
+            </p>
+            <p>
+              {user?.firstName} {user?.lastName}
+            </p>
+            <p>
+              <span className="font-brand__font__regular">Email:</span>{" "}
+              {user?.email}
+            </p>
+            <p className="content-gap">
+              {user?.phone && (
+                <>
+                  <span className="font-brand__font__regular">Phone:</span>{" "}
+                  {user?.phone}
+                </>
+              )}
+            </p>
+          </div>
 
-          <div
+          <button
             onClick={() => {
               handleEdit("profile");
             }}
-            className=" border-b-2 border-teagreen-500 px-[1px] w-fit cursor-pointer hover:text-teagreen-600"
+            className="border-b-2 border-teagreen-500 px-[1px] w-fit h-fit cursor-pointer hover:text-teagreen-600 font-brand__font__regular"
           >
-            Edit
-          </div>
+            Edit profile
+          </button>
         </div>
 
         {/* Password Section */}
@@ -93,35 +107,31 @@ function AddressScreen() {
           onClick={() => {
             handleEdit("password");
           }}
-          className="capitalize flex gap-2 border-b-2 border-teagreen-500 px-[1px] w-fit cursor-pointer hover:text-teagreen-600"
+          className="flex gap-2 border-b-2 border-teagreen-500 px-[1px] w-fit cursor-pointer hover:text-teagreen-600 font-brand__font__regular"
         >
           <span className="pt-1 text-sm">
             <GoKey />
           </span>{" "}
-          Change Password
+          Change password
         </div>
       </div>
-      <div className="p-6 bg-teagreen-100 rounded-lg">
-        <div className="flex justify-between gap-4">
-          <p className="text-2xl content-gap">Your Address</p>
+
+      <div className="p-6 bg-teagreen-200 rounded">
+        <div className="flex items-start justify-between gap-4 border">
+          <p className="text-brand__font__size__lg mb-1 content-gap">Your Address</p>
           <div
             onClick={() => {
               handleEdit("addAddress");
             }}
-            className="flex gap-2 cursor-pointer hover:text-teagreen-600"
+            className="cursor-pointer hover:text-teagreen-600 group"
           >
-            <div className="flex items-center justify-center pb-[1px] w-7 h-7 text-[2.5rem] font-thin bg-white text-gray-800 rounded-full">
-              +
-            </div>
-            <div className="relative text-gray-800">
-              <span className="block hover:text-teagreen-600">
-                Add New Address
-              </span>
-              <span className="block w-full h-[1.5px] bg-teagreen-500 mt-[-2px]"></span>
+            <div className="text-gray-800 h-fit border-b-2 border-teagreen-500 flex gap-1 items-center">
+              <FiPlusCircle className="text-gray-800 group-hover:text-teagreen-600" />
+              <span className="hover:text-teagreen-600">Add new address</span>
             </div>
           </div>
         </div>
-        <div className="">
+        <div>
           {allAddressData
             ?.slice()
             .sort((a, b) => (b.isDefault ? 1 : 0) - (a.isDefault ? 1 : 0))
@@ -167,7 +177,7 @@ function AddressScreen() {
               </div>
             ))}
         </div>
-        <div className="flex justify-center mt-6 text-teagreen-600">
+        <div className="flex text-teagreen-600 text-brand__font__size__sm">
           <span>At most 5 addresses can be added!</span>
         </div>
       </div>
