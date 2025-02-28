d35af1eadbd814135f7e26be0dc98938447cb777
dipto <dipto.bd.r@gmail.com>
2025-03-01 02:33:20 +0600

    Redeem rewards done

---
+++ services/features/reward/rewardApi.js
@@ -0,0 +1,17 @@
+import { api } from "@/services/api/apiSlice";
+import generateServiceUrl from "@/utils/generateServiceUrl";
+
+const rewardApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    postRewards: builder.mutation({
+      query: ({ data }) => ({
+        url: generateServiceUrl("/secure/reward/redeem"),
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["reward"],
+    }),
+  }),
+});
+
+export const { usePostRewardsMutation } = rewardApi;
