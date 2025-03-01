c98d5fa439150448f6f8ec91edcc68fc9378e293
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2025-03-01 11:02:44 +0600

    parsing html coming from DB with html-parser

--- components/subscriptionPolicy/index.jsx
+++ components/subscriptionPolicy/index.jsx
@@ -2,14 +2,11 @@
 
 import "@/styles/editor.css";
 import DOMPurify from "dompurify";
+import HtmlParser from "react-html-parser";
 
 const SubscriptionPolicyContent = ({ data }) => {
   const sanitizedContent = DOMPurify.sanitize(data);
-  return (
-    <div className='liststyle'>
-      <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
-    </div>
-  );
+  return <div className='quill-classNames'>{HtmlParser(sanitizedContent)}</div>;
 };
 
 export default SubscriptionPolicyContent;
