45bb5837d0a9fed4162a55e19941d8f0a2f21dae
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2025-03-01 09:04:28 +0600

    styles for list items redefined

--- components/termsconditions/index.js
+++ components/termsconditions/index.js
@@ -1,16 +1,11 @@
 "use client";
-
-import "@/styles/editor.css";
 import DOMPurify from "dompurify";
+import HtmlParser from "react-html-parser";
 
 const TermsConditionsContents = ({ data }) => {
   const sanitizedContent = DOMPurify.sanitize(data);
 
-  return (
-    <div className='liststyle'>
-      <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
-    </div>
-  );
+  return <div className='quill-classNames'>{HtmlParser(sanitizedContent)}</div>;
 };
 
 export default TermsConditionsContents;
