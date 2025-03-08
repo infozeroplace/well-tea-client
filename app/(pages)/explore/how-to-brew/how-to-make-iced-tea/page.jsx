23b9aeff28fae75b3994a4c6e68a689bbfc708e0
Hridoy <hridoy007.lu@gmail.com>
2025-03-08 14:43:52 +0600

    How to brew pages have updated

--- app/(pages)/explore/how-to-brew/how-to-make-iced-tea/page.jsx
+++ app/(pages)/explore/how-to-brew/how-to-make-iced-tea/page.jsx
@@ -14,13 +14,13 @@ const MakeIcedTea = () => {
   return (
     <div>
       <CommonBanner bannerTitle="How to make iced tea" />
-      <div className="container px-4 lg:px-20">
-        <div className="section-gap mt-20">
+      <div className="container px-4 lg:px-20 mt-20">
+        {/* <div className="section-gap mt-20">
           <div className="text-5xl font-semibold tracking-tight content-gap">
             How to make <br />
             iced tea
           </div>
-        </div>
+        </div> */}
 
         {/* Heading Section */}
         <div className="section-gap text-2xl text-center lg:text-left tracking-tight font-medium">
