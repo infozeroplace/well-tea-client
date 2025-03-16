fc5b979ebede2fbf26609f0c7e7a014859c1a509
dipto <dipto.bd.r@gmail.com>
2025-03-16 17:15:14 +0600

    Category offer image updated

--- components/homepage/categoryoffer/CategoryOfferCard.jsx
+++ components/homepage/categoryoffer/CategoryOfferCard.jsx
@@ -6,17 +6,17 @@ const CategoryOfferCard = ({ title, subTitle, thumbnail = [], url }) => {
   return (
     <Link
       href={url}
-      className={`rounded-lg group relative inline-block overflow-hidden`}
-      // style={{
-      //   backgroundImage: `url(${env.app_url}${thumbnail[0]?.filepath})`,
-      // }}
+      className={`rounded-lg group relative bg-cover bg-no-repeat inline-block overflow-hidden`}
+      style={{
+        backgroundImage: `url(${env.app_url}${thumbnail[0]?.filepath})`,
+      }}
     >
-      <NextImage
+      {/* <NextImage
         img={`${env.app_url}${thumbnail[0]?.filepath}`}
         alt={`${thumbnail[0]?.alternateText}`}
-        presets={{width: 300, height: 350}}
-        className= "absolute top-0 left-0 object-cover"
-      />
+        presets={{width: 300, height: 400}}
+        className= "absolute top-0 left-0 object-contain w-full h-full"
+      /> */}
       <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-black opacity-50 rounded-lg"></div>
       <div className="text-white flex flex-col justify-center items-center text-center h-[500px] relative">
         <div className="text-3xl group-hover:text-4xl duration-300 capitalize">
